3ced66d4c5f4a8f9bd47daa150eff9e5a96059ff
Noah Phom <nampdn@protonmail.com>
2019-05-27 08:39:12 +0000

    fix(deploy): change https

--- deploy.sh
+++ deploy.sh
@@ -96,7 +96,7 @@ deploy() {
 	sleep $WAIT_FOR_IPFS
 	swarm_connect
   hash=$(ipfs add -w -r ./scripts)
-  update_dns "https://scripts.barajs.dev" $hash
+  update_dns "scripts.barajs.dev" $hash
   echo "Added script ${IPFS_SCRIPTS[i]} to IPFS with hash: $hash"
 	ipfs shutdown
 }
