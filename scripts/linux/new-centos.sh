cf30701a790f6ed5601552477d2a92965c0e1cb0
Noah Phom <nampdn@protonmail.com>
2019-06-04 15:04:47 +0700

    Update new-centos.sh

--- scripts/linux/new-centos.sh
+++ scripts/linux/new-centos.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Automated set up a new linux Ubuntu machine
+# Automated set up a new linux Centos machine
 # 1. Install CURL (if not yet existed)
 # 2. Command: curl -sSL https://scripts.barajs.dev/linux/new-centos.sh | bash
 
 setup() {
   # Prepare the machine
   yum -y install epel-release yum-plugin-priorities \
-https://download.ceph.com/rpm-luminous/el7/noarch/ceph-release-1-1.el7.noarch.rpm
+  https://download.ceph.com/rpm-luminous/el7/noarch/ceph-release-1-1.el7.noarch.rpm
   sed -i -e "s/enabled=1/enabled=1\npriority=1/g" /etc/yum.repos.d/ceph.repo
   yum -y update
 
   # Install dependencies
-  yum install -y git zsh tmux nmon nload htop
+  yum install -y git zsh tmux nmon nload htop python-setuptools python2
 
   # Install core softwares
   ## ZSH
