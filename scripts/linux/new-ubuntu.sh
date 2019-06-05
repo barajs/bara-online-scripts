a4dbe4ea8262e39c00e37090bfc799cdb302bfb3
Noah Phom <nampdn@protonmail.com>
2019-06-05 15:39:56 +0700

    Update new-ubuntu.sh

--- scripts/linux/new-ubuntu.sh
+++ scripts/linux/new-ubuntu.sh
@@ -17,12 +17,6 @@ setup() {
   ## ZSH
   echo "Installing OH-MY-ZSH..."
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  
-  echo "Installing OH-MY-ZSH basic plugins..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
-  sed -i 's/\(git\)/git zsh-syntax-highlighting zsh-autosuggestions docker/' ~/.zshrc && \
-  source ~/.zshrc
 }
 
 main() {
