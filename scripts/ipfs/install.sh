29d190c0cac98aac4b2ae3881b8444d730fa110e
Noah Phom <nampdn@protonmail.com>
2019-06-13 17:24:24 +0700

    Create install.sh

---
+++ scripts/ipfs/install.sh
@@ -0,0 +1,13 @@
+# Install IPFS Binary
+# Command: curl https://scripts.barajs.dev/ipfs/install.sh | bash -s -- <version|optional>
+# Command: curl https://scripts.barajs.dev/ipfs/install.sh | bash -s -- "v0.4.21"
+
+version=$1
+IPFS_VERSION=${version:-"v0.4.21"}
+IPFS_BIN="go-ipfs_${IPFS_VERSION}_linux-386.tar.gz"
+wget https://dist.ipfs.io/go-ipfs/$IPFS_VERSION/$IPFS_BIN && \
+tar xvfz $IPFS_BIN && \
+sudo mv go-ipfs/ipfs /usr/local/bin/ipfs && \
+rm -rf go-ipfs $IPFS_BIN
+
+echo "GO-IPFS ${IPFS_VERSION} installed!"
